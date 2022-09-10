c8c943573b6a42601a7b4bae380cd64c9a62857e
michalskimateusz <michalskimateusz@outlook.com>
2022-09-10 21:06:16 +0200

    fetching from API

---
+++ src/Components/Videos.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import { Box, Stack } from "@mui/material";
+import { ChannelCard, VideoCard } from "./index";
+
+const Videos = ({ videos }) => {
+  return (
+    <Stack direction="row" flexWrap="wrap" justifyContent="start" gap={2}>
+      {videos.map((item, index) => (
+        <Box key={index}>
+          {item.id.videoId && <VideoCard video={item} />}
+          {item.id.channelId && <ChannelCard channelDetail={item} />}
+        </Box>
+      ))}
+    </Stack>
+  );
+};
+
+export default Videos;
